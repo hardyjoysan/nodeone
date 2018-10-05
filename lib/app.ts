4abb993bf11e850978882f98eaac1e98554eae33
Hardy Joysan <hardyjoysan@gmail.com>
2018-10-05 15:42:08 +0530

    Basic routing done

--- lib/app.ts
+++ lib/app.ts
@@ -1,13 +1,16 @@
 import * as express from "express";
 import * as bodyparser from "body-parser";
+import {Routes} from "./routes/crmRoutes";
 
 class App{
 
     public app: express.Application;
+    public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
         this.config();
+        this.routePrv.routes(this.app);
     }
 
     private config(): void{
---
+++ lib/routes/crmRoutes.ts
@@ -0,0 +1,12 @@
+import {Request, Response} from "express";
+
+export class Routes{
+    public routes(app): void{
+        app.route('/')
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'GET request successfull.'
+            })
+        })
+    }
+}
