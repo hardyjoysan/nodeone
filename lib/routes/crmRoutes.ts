f265eee6add195fb16b70635b1cd6d6feb7d569c
Hardy Joysan <hardyjoysan@gmail.com>
2018-10-05 16:35:34 +0530

    Basic CRUD routes added

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -8,5 +8,43 @@ export class Routes{
                 message: 'GET request successfull.'
             })
         })
+
+        //Contact
+        app.route('/contact')
+        //GET - All Contacts
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'Get request successfull.'
+            })
+        })
+        //POST - Create new contact
+        .post((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'POST request successfull.'
+            })
+        })
+
+        // Contact detail
+        app.route('/contact/:contactId')
+        // GET - specific contact
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'GET request successful'
+            })
+        })
+        // PUT - Update a contact
+        .put((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'PUT request successful'
+            })
+        })
+        // DELETE a contact
+        .delete((req: Request, res: Response) => {       
+            res.status(200).send({
+                message: 'DELETE request successful'
+            })
+        })
+
+
     }
 }
