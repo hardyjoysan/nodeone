3e2c81e2c6597f6fe59a991e778bdc3174dd8c15
Hardy Joysan <hardyjoysan@gmail.com>
2018-10-08 10:29:05 +0530

    MongoDB mongoose connected to app

--- lib/app.ts
+++ lib/app.ts
@@ -1,16 +1,19 @@
 import * as express from "express";
 import * as bodyparser from "body-parser";
 import {Routes} from "./routes/crmRoutes";
+import * as mongoose from "mongoose";
 
 class App{
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://root:hardy12345@ds223253.mlab.com:23253/nodeone';
 
     constructor() {
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -19,6 +22,11 @@ class App{
 
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyparser.urlencoded({ extended : false }));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true });
     }
 }
 
